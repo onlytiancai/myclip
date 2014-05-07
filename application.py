5542e318b3f121de854d8789cef7a2519a112e88
huhao <huhao@dnspod.com>
2014-05-07 11:04:43 +0800

    增加历史查看功能

--- application.py
+++ application.py
@@ -2,6 +2,7 @@
 import os
 import web
 from datetime import datetime
+from collections import defaultdict
 
 tpl_dir = os.path.join(os.path.dirname(__file__), 'templates')
 render = web.template.render(tpl_dir, base='layout')
@@ -12,11 +13,14 @@ form = web.form.Form(web.form.Textbox("title", web.form.notnull, description=u"�
                      web.form.Button("submit", type="submit", description="保存"),
                      )
 
+def show_error(message, backurl='/'):
+    return web.seeother('/showerror?message=%s&backurl=%s' % (message, backurl))
 
 class Index(object):
     def GET(self):
-        clips = db.select('clips', order="changed_time desc")
+        clips = db.select('clips', order="changed_time desc", where="pid = 0")
         return render.index(clips)
+
 
 class New(object):
     def GET(self):
@@ -27,7 +31,9 @@ class New(object):
         if f.validates():
             db.insert('clips', title=f.d.title, content=f.d.content,
                       created_time=datetime.now(), changed_time=datetime.now())
-        return web.seeother('/')
+            return web.seeother('/')
+        else:
+            return show_error(u'标题和内容不能为空')
 
 
 class Show(object):
@@ -49,9 +55,18 @@ class Edit(object):
     def POST(self, id):
         f = form()
         if f.validates():
-            db.update('clips', where="id=$id", vars=dict(id=id),
-                      changed_time=datetime.now(), title=f.d.title, content=f.d.content)
-        return web.seeother('/show/%s' % id)
+            clip = db.select('clips', where='id=$id', vars=dict(id=id))
+            if clip:
+                row = clip[0] # INFO:
+                db.insert('clips', pid=id, title=row.title, content=row.content,
+                          created_time=datetime.now(), changed_time=datetime.now())
+                db.update('clips', where="id=$id", vars=dict(id=id),
+                          changed_time=datetime.now(), title=f.d.title, content=f.d.content)
+                return web.seeother('/show/%s' % id)
+            else:
+                return web.notfound()
+        else:
+            return show_error(u'标题和内容不能为空', '/edit/%s' % id)
 
 
 class Delete(object):
@@ -63,8 +78,39 @@ class Delete(object):
             return web.notfound()
 
     def POST(self, id):
-        db.delete('clips', where="id=$id", vars=locals())
+        db.delete('clips', where="id=$id or pid=$id", vars=dict(id=id))
         return web.seeother('/')
+
+
+class ShowError(object):
+    def GET(self):
+        return render.showerror(web.input())
+
+
+class HistoryList(object):
+    def GET(self, id):
+        input = web.input(page=1)
+        page = int(input.page)
+        if page < 1: 
+            page = 1
+        pagesize = 100 
+        offset = (page - 1) * pagesize 
+        clip = db.select('clips', where='id=$id', vars=locals())
+        if clip:
+            rows = db.select('clips', where="pid=$pid", vars=dict(pid=id), 
+                              order="changed_time desc", limit=pagesize, offset=offset)
+            group_clips = defaultdict(list)
+            for row in rows:
+                group_clips[row.changed_time.strftime('%Y-%m-%d')].append(row)
+            return render.historylist(clip[0], group_clips, page)
+        else:
+            return web.notfound()
+
+class HistoryShow(object):
+    def GET(self, pid, id):
+        pclip = db.select('clips', where='id=$pid', vars=dict(pid=pid))
+        clip = db.select('clips', where='id=$id', vars=dict(id=id))
+        return render.historyshow(pclip[0], clip[0])
 
 
 
@@ -73,6 +119,9 @@ urls = ["/", Index,
         "/show/(\d+)", Show,
         "/edit/(\d+)", Edit, 
         "/delete/(\d+)", Delete, 
+        "/showerror", ShowError, 
+        "/historylist/(\d+)", HistoryList,
+        "/historyshow/(\d+)/(\d+)", HistoryShow,
         ]
 
 app = web.application(urls, globals())
