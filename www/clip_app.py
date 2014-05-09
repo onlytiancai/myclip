aeb8e71288ec6c97e309864c55c7f5fe16bc4d2f
huhao <huhao@dnspod.com>
2014-05-09 13:46:35 +0800

    修改时可以填写概要信息，在历史里可以查看，去掉一些出错判断逻辑

--- www/clip_app.py
+++ www/clip_app.py
@@ -7,7 +7,7 @@ import settings
 import utils
 
 db = web.database(**settings.DB_CONN)
-render = utils.render
+render = utils.render.clip
 
 
 class Index(object):
@@ -22,7 +22,9 @@ class New(object):
 
     def POST(self):
         i = web.input()
-        db.insert('clips', title=i.title, content=i.content,
+        new_id = db.insert('clips', title=i.title, content=i.content,
+                           created_time=datetime.now(), changed_time=datetime.now())
+        db.insert('clips', pid=new_id, title=i.title, content=i.content,
                   created_time=datetime.now(), changed_time=datetime.now())
         return web.seeother('/')
 
@@ -30,41 +32,28 @@ class New(object):
 class Show(object):
     def GET(self, id):
         clip = db.select('clips', where='id=$id', vars=locals())
-        if clip:
-            return render.show(clip[0])
-        else:
-            return web.notfound()
+        return render.show(clip[0])
 
 
 class Edit(object):
     def GET(self, id):
         clip = db.select('clips', where='id=$id', vars=locals())
-        if clip:
-            return render.edit(clip[0])
-        else:
-            return web.notfound()
+        return render.edit(clip[0])
 
     def POST(self, id):
-        i = web.input()
-        clip = db.select('clips', where='id=$id', vars=dict(id=id))
-        if clip:
-            row = clip[0]  # INFO:
-            db.insert('clips', pid=id, title=row.title, content=row.content,
-                      created_time=datetime.now(), changed_time=datetime.now())
-            db.update('clips', where="id=$id", vars=dict(id=id),
-                      changed_time=datetime.now(), title=i.title, content=i.content)
-            return web.seeother('/show/%s' % id)
-        else:
-            return web.notfound()
+        i = web.input(title=u'无概要信息')
+        db.update('clips', where="id=$id", vars=dict(id=id),
+                  changed_time=datetime.now(), content=i.content)
+
+        db.insert('clips', pid=id, title=i.title, content=i.content,
+                  created_time=datetime.now(), changed_time=datetime.now())
+        return web.seeother('/show/%s' % id)
 
 
 class Delete(object):
     def GET(self, id):
         clip = db.select('clips', where='id=$id', vars=locals())
-        if clip:
-            return render.delete(clip[0])
-        else:
-            return web.notfound()
+        return render.delete(clip[0])
 
     def POST(self, id):
         db.delete('clips', where="id=$id or pid=$id", vars=dict(id=id))
@@ -85,15 +74,12 @@ class HistoryList(object):
         pagesize = 100
         offset = (page - 1) * pagesize
         clip = db.select('clips', where='id=$id', vars=locals())
-        if clip:
-            rows = db.select('clips', where="pid=$pid", vars=dict(pid=id),
-                             order="changed_time desc", limit=pagesize, offset=offset)
-            group_clips = defaultdict(list)
-            for row in rows:
-                group_clips[row.changed_time.strftime('%Y-%m-%d')].append(row)
-            return render.historylist(clip[0], group_clips, page)
-        else:
-            return web.notfound()
+        rows = db.select('clips', where="pid=$pid", vars=dict(pid=id),
+                         order="changed_time desc", limit=pagesize, offset=offset)
+        group_clips = defaultdict(list)
+        for row in rows:
+            group_clips[row.changed_time.strftime('%Y-%m-%d')].append(row)
+        return render.historylist(clip[0], group_clips, page)
 
 
 class HistoryShow(object):
